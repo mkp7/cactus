a325e2896180716c547e71504bb6e226fccefdd1
achyuthreddy <ncachyuth@gmail.com>
2023-03-05 14:05:36 +0530

    feat: WIP adds new collection form

--- ui/src/components/sidecar/NewCollection.tsx
+++ ui/src/components/sidecar/NewCollection.tsx
@@ -1,11 +1,95 @@
 import React from 'react'
+import { useForm, useFieldArray, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Button } from '../../elements'
+import * as yup from 'yup'
 
-
+const schema = yup.object().shape({
+  space_id: yup.number().required(),
+  creator_id: yup.number().required(),
+  name: yup
+    .string()
+    .trim()
+    .required('Please provide the channel name!')
+		.min(1)
+    .max(
+      100,
+      `Please keep it under 100 characters`
+    ),
+  schema: yup.array().of(yup.object().shape({
+		name: yup.string().required(),
+		type: yup.string().required(),
+		required: yup.boolean().required(),
+    unique: yup.boolean().required()
+	}).required())
+})
 
 const NewCollection = (props : {}) => {
+	const { register, control, handleSubmit, reset, trigger, setError } = useForm({
+    // defaultValues: {}; you can populate the fields by this attribute 
+  });
+	const { fields, append, remove } = useFieldArray({
+    control,
+    name: "schema"
+  });
+	
   return (
 			<div className="px-4 py-8">
-				<h1>hi</h1>
+				<form autoComplete="on" onSubmit={(e) => {e.preventDefault()}} className='flex flex-col items-center'>
+					<table className='w-full'>
+						<tr className='flex'>
+							<th className='grow'>Name</th>
+							<th className='grow'>Type</th>
+							<th className='w-36'>Required?</th>
+							<th>Unique?</th>
+						</tr>
+							{fields.map((item, index) => (
+								<tr key={index} className='flex space-x-2 justify-between'>
+									<td>
+										<Controller
+											render={({ field }) => <input className='grow' {...field} />}
+											name={`schema.${index}.name`}
+											control={control}
+										/>
+											</td>
+											<td>
+											<Controller
+											render={({ field }) => (
+												<select className='grow w-48' {...field}>
+                          <option value='text'>Text</option>
+                          <option value='number'>Number</option>
+                          <option value='boolean'>Boolean</option>
+                        </select>
+											)}
+											name={`schema.${index}.type`}
+											control={control}
+										/>
+											</td>
+											<td>
+											<Controller
+												render={({ field }) => <input 	type="checkbox" className='w-36' {...field} />}
+												name={`schema.${index}.required`}
+												control={control}
+											/>
+											</td>
+											<td>
+											<Controller
+												render={({ field }) => <input 	type="checkbox" className='' {...field} />}
+												name={`schema.${index}.unique`}
+												control={control}
+											/>
+											</td>
+									</tr>
+								))}
+					</table>
+					<Button
+						type="button"
+						classname="mx-auto"
+						onClick={() => append({ firstName: "bill", lastName: "luo" })}
+					>
+						Add Field
+					</Button>
+        </form>
 			</div>
   )
 }
